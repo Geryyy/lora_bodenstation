9ca9bc7d3643576fc568e68505b364fbf457fb68
Gerald Ebmer <gerald.ebmer@outlook.com>
2018-08-24 10:46:43 +0200

    serialRadioTunnel OK

--- main.cpp
+++ main.cpp
@@ -13,40 +13,77 @@ RESET   PA_8
 INT     PA_0
 */
 
+signed char smp_frameReady(fifo_t* buffer);
+void serialRadioTunnel(void);
+
+Serial pc(USBTX, USBRX, 115200);
+Thread radioThread;
+RFM98W radio(PB_5, PB_4, PB_3, PB_10, PA_8, PA_0, 2, smp_frameReady, NULL, false);
+
 signed char smp_frameReady(fifo_t* buffer) //Frame wurde empfangen
 {
     int32_t len = fifo_datasize(buffer);
-    printf("SMP-Frame received!!!\n\t");
+    // printf("SMP-Frame received!!!\n\t");
     for(int i = 0; i<len; i++){
         uint8_t ch;
         fifo_read_byte(&ch,buffer);
-        printf("%c",ch);
+        pc.putc(ch);
     }
-    printf("\n\n");
     return len;
 }
 
-Thread radioThread;
-RFM98W radio(PB_5, PB_4, PB_3, PB_10, PA_8, PA_0, 10, smp_frameReady, NULL, false);
 
 void radioTask(){
     while(true){
         radio.serviceRadio();
-        wait(1);
+        wait(0.2);
     }
 }
 
-Serial pc(USBTX, USBRX, 115200);
+
 
 int main(){
     printf("Lora Bodenstation V0.1\nGerald Ebmer 2018\n\n");    
-    char msg[] = "Hello World!\n";
     radioThread.start(radioTask);
+    serialRadioTunnel();
 
     while(true){
-        wait(3);
-        printf("transmit data\n");
-        radio.sendPacket(msg,sizeof(msg));
-        // radio.startreceive();
+        // shoud not reach this point
+    }
+}
+
+
+#define PACKET_LENGTH 24
+
+void serialRadioTunnel(){
+    static time_t time1;
+    time_t TIMEOUT = 3;
+    char msg[PACKET_LENGTH];
+    static int i = 0;
+
+    time1 = time(NULL);
+
+    while(true){
+        if(pc.readable() == true){
+            msg[i] = pc.getc();
+            if(i==0){
+                time1 = time(NULL);
+            }
+            i++;
+        }
+
+        if(i>=PACKET_LENGTH){
+            radio.sendPacket(msg,PACKET_LENGTH);
+            i = 0;
+            time1 = time(NULL);
+            // printf("full packet\n");
+        }
+
+        if( ((time(NULL)-time1) > TIMEOUT) && (i > 0) ){
+            radio.sendPacket(msg,i);
+            i = 0;
+            time1 = time(NULL);
+            // printf("Timeout\n");
+        }
     }
 }
